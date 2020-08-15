3efdc9918f323f8cc2fb7f753773456491291a0f
VladGesin <gasinvlad@gmail.com>
2020-08-15 22:17:05 +0300

    add slider and data box

--- src/components/layout/MainWin/NavBar/navbar.js
+++ src/components/layout/MainWin/NavBar/navbar.js
@@ -1,91 +1,53 @@
-import React, { Component, Fragment } from 'react'
-import './navbar.css'
+import React, { Fragment } from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import './navbar.css';
 
 const navbar = () => {
-        return (
-          <Fragment>
-            <nav class="navbar navbar-expand-lg navbar-light">
-              <button
-                class="navbar-toggler ml-auto"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarNavDropdown"
-                aria-controls="navbarNavDropdown"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                  <li class="nav-item active">
-                    <a class="nav-link" href="#">
-                      אודות <span class="sr-only">(current)</span>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">
-                      מפות
-                    </a>
-                  </li>
-                  <li class="nav-item dropdown" dir="rtl">
-                    <a
-                      class="nav-link dropdown-toggle"
-                      href="#"
-                      id="navbarDropdownMenuLink"
-                      data-toggle="drop"
-                      aria-haspopup="true"
-                      aria-expanded="false"
-                    >
-                      יושבי ראש
-                    </a>
-                    <div
-                      class="dropdown-menu"
-                      aria-labelledby="navbarDropdownMenuLink"
-                    >
-                      <a class="dropdown-item" href="#">
-                        Action
-                      </a>
-                      <a class="dropdown-item" href="#">
-                        Another action
-                      </a>
-                      <a class="dropdown-item" href="#">
-                        Something else here
-                      </a>
-                    </div>
-                  </li>
-                  <li class="nav-item dropdown" dir='rtl'>
-                    <a
-                      class="nav-link dropdown-toggle"
-                      href="#"
-                      id="navbarDropdownMenuLink"
-                      data-toggle="dropdown"
-                      aria-haspopup="true"
-                      aria-expanded="false"
-                    >
-                      ועדות
-                    </a>
-                    <div
-                      class="dropdown-menu"
-                      aria-labelledby="navbarDropdownMenuLink"
-                    >
-                      <a class="dropdown-item" href="#">
-                        Action
-                      </a>
-                      <a class="dropdown-item" href="#">
-                        Another action
-                      </a>
-                      <a class="dropdown-item" href="#">
-                        Something else here
-                      </a>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-          </Fragment>
-        );
-    }
+  return (
+    <Fragment>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light" dir="rtl">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav " />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto ">
+            <NavDropdown
+              title="יושב ראש"
+              id="collasible-nav-dropdown"
+              dir="rtl"
+              className="ml-auto"
+            >
+              <NavDropdown.Item href="#action/3.1">
+                יושב ראש לדוגמא
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                יושב ראש שני
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                יושב ראש שלישי
+              </NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown
+              className="ml-auto"
+              title="ועדות"
+              id="collasible-nav-dropdown"
+              dir="rtl"
+            >
+              <NavDropdown.Item href="#action/3.1">
+                ועדה לדוגמא
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">ועדה שני</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">ועדה שלישי</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link className="ml-auto" href="#features">
+              אודות
+            </Nav.Link>
+            <Nav.Link className="ml-auto" href="#pricing">
+              מפות
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Fragment>
+  );
+};
 
-
-export default navbar
+export default navbar;
