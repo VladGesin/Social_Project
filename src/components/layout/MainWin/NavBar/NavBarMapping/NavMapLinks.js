958f77a29492a96af95d39f423c02731dd7f0b99
VladGesin <gasinvlad@gmail.com>
2020-09-08 18:15:37 +0300

    fix navbar with galia

--- src/components/layout/MainWin/NavBar/NavBarMapping/NavMapLinks.js
+++ src/components/layout/MainWin/NavBar/NavBarMapping/NavMapLinks.js
@@ -6,8 +6,17 @@ const NavMapLinks = (props) => {
   return (
     <Fragment>
       <li>
+        {console.log(props.link.item)}
         <NavDropdown.Item className="text-right">
-          <NavLink to={props.link.path}>{props.link.name}</NavLink>
+          {/* NavLink */}
+          <NavLink
+            to={{
+              pathname: `${props.link.path}/:${props.link.item}`,
+              state: { item: props.link.item },
+            }}
+          >
+            {props.link.name}
+          </NavLink>
         </NavDropdown.Item>
       </li>
     </Fragment>
