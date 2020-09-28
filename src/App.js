50ba0b89d11f79d710e32733c8d2d4440a8c3e1d
VladGesin <gasinvlad@gmail.com>
2020-09-28 14:50:39 +0300

    MainProject

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+import Login from './components/layout/Login/Login';
+import MainWin from './components/layout/MainWin/mainWin';
+import './css/commitmap.css';
+
+const App = () => {
+  return (
+    <Fragment>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/MainWin" component={MainWin} />
+        </Switch>
+      </Router>
+    </Fragment>
+  );
+};
+
+export default App;
---
+++ src/components/layout/MainWin/WelcomPage/WelcomPage.js
@@ -0,0 +1,38 @@
+import React, { Fragment } from 'react';
+import Slider from './Slider/Slider';
+import Container from 'react-bootstrap/Container';
+import GoodWord from '../WelcomPage/GoodWord/GoodWord';
+import News from '../WelcomPage/News/News';
+import Birthday from '../WelcomPage/Birthdays/Birthday';
+import Teachers from '../WelcomPage/PrivateTeacher/PrivateTeacher';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+
+import './WelcomPage.css';
+
+const WelcomPage = () => {
+  return (
+    <Fragment>
+      <Container>
+        <div className="row justify-content-md-center">
+          <div className="col-md-8 ">
+            <div className="row d-block ">
+              <Slider />
+            </div>
+            <div className="row d-block  ">
+              <GoodWord />
+            </div>
+          </div>
+
+          <div className="col">
+            <News />
+            <Birthday />
+            <Teachers />
+          </div>
+        </div>
+      </Container>
+    </Fragment>
+  );
+};
+
+export default WelcomPage;
