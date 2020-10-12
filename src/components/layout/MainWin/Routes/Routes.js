9897b925131f0118a1567731d6ef974c67eddcfd
Ron Benaish <r.benaish@gmail.com>
2020-10-12 10:39:30 +0300

    adding xpertesy page

--- src/components/layout/MainWin/Routes/Routes.js
+++ src/components/layout/MainWin/Routes/Routes.js
@@ -1,35 +1,38 @@
-import React, { Fragment } from 'react';
-import WelcomPage from '../../MainWin/WelcomPage/WelcomPage';
-import commiteesWin from '../../CommiteesWin/commiteesWin';
-import { Route } from 'react-router-dom';
-import chairmanItems from '../../ChairmanItems/ChairmanItems';
-import secreturywin from '../../SecretaryWin/SecreturyWin'
+import React, { Fragment } from "react";
+import WelcomPage from "../../MainWin/WelcomPage/WelcomPage";
+import commiteesWin from "../../CommiteesWin/commiteesWin";
+import { Route } from "react-router-dom";
+import chairmanItems from "../../ChairmanItems/ChairmanItems";
+import secreturywin from "../../SecretaryWin/SecreturyWin";
+import NewMeeting from "../../NewMeeting/NewMeeting";
 
 const COMPONENT_MAP = {
-  WelcomPage: WelcomPage,
-  commiteesWin: commiteesWin,
-  chairmanItems: chairmanItems,
-  secreturywin:secreturywin
+   WelcomPage: WelcomPage,
+   commiteesWin: commiteesWin,
+   chairmanItems: chairmanItems,
+   secreturywin: secreturywin,
+   NewMeeting: NewMeeting,
 };
 
 // const COMPONENT_MAP = menuItems.map((item)=>)
 
 const Routes = () => {
-  const menuItems = [
-    { name: 'WelcomPage', path: '/MainWin' },
-    { name: 'commiteesWin', path: '/commiteesWin/:education' },
-    { name: 'commiteesWin', path: '/commiteesWin/:sport' },
-    { name: 'chairmanItems', path: '/chairmanItems/:item' },
-    { name: 'secreturywin', path: '/secreturywin' },
-  ];
-  return (
-    <Fragment>
-      {menuItems.map((item) => (
-        //COMPONENT_MAP[item.name] returns componentName
-        <Route path={item.path} component={COMPONENT_MAP[item.name]} />
-      ))}
-    </Fragment>
-  );
+   const menuItems = [
+      { name: "WelcomPage", path: "/MainWin" },
+      { name: "commiteesWin", path: "/commiteesWin/:education" },
+      { name: "commiteesWin", path: "/commiteesWin/:sport" },
+      { name: "chairmanItems", path: "/chairmanItems/:item" },
+      { name: "secreturywin", path: "/secreturywin" },
+      { name: "NewMeeting", path: "/newmeeting" },
+   ];
+   return (
+      <Fragment>
+         {menuItems.map((item) => (
+            //COMPONENT_MAP[item.name] returns componentName
+            <Route path={item.path} component={COMPONENT_MAP[item.name]} />
+         ))}
+      </Fragment>
+   );
 };
 
 export default Routes;
