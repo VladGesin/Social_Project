a143cb42e24a9dfa8573dc0018868f6c25ccc563
idanhakim <idanhakim123@gmail.com>
2020-10-28 00:13:14 +0200

    update routes file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{useContext} from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Login from './components/layout/Login/Login';
 import MainWin from './components/layout/MainWin/mainWin';
@@ -8,14 +8,6 @@ import Context from "./store/Context";
 const App = () => {
    const context = useContext(Context)
    return (
-    //   <ContextProvider>
-    //       <Router>
-    //           <Switch>
-    //               <Route path="/Social_Project/" exact component={Login} />
-    //               <Route path="/Social_Project/MainWin" component={MainWin} />
-    //           </Switch>
-    //       </Router>
-    //   </ContextProvider>
     <Router>
       {context.userState.isAuth?<MainWin/> : <Login/>}
     </Router>
