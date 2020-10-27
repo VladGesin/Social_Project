49f20fd7272d8f56593862191ed58a04ca8b35e5
idan <idan@r2net.com>
2020-10-27 15:22:12 +0200

    add table to main committees page

--- src/components/layout/MainCommitteesPage/MainCommitteesPage.jsx
+++ src/components/layout/MainCommitteesPage/MainCommitteesPage.jsx
@@ -2,22 +2,43 @@ import React from "react";
 import styles from "./MainCommitteesPage.module.scss";
 import {NavLink} from "react-router-dom";
 import {committeesConfig} from "./committeesConfig";
+import { Table} from 'antd';
+
 
 
 export const MainCommitteesPage = () => {
 
+    const columns = [
+        {
+            title: 'שם ועדה',
+            dataIndex: 'name',
+            key: 'name',
+            align: 'right',
+            render: (text, {url}) => <NavLink to={url}>{text}</NavLink>
+        },
+        {
+            title: 'פירוט',
+            dataIndex: 'desc',
+            key: 'desc',
+            align: 'right',
+        }
+    ];
+
+    const _data= committeesConfig.map((item, i) =>{
+        return ({
+            key: i+1,
+            name: item.name,
+            desc: item.desc,
+            url: `/Social_Project/committees/${item.paramKey}`
+        });
+    })
+
     return (
         <div className={styles.rootMainCommitteesPage}>
-            <h1>מסך ועדות ראשי</h1>
-            <ul>
-                {committeesConfig.map(({paramKey, name}, i) => {
-                    return (
-                        <li key={i}>
-                            <NavLink to={`/Social_Project/committees/${paramKey}`}>{name}</NavLink>
-                        </li>
-                    );
-                })}
-            </ul>
+            <h2>מסך ועדות ראשי</h2>
+            <div className={styles.tableWrapper} dir={'rtl'}>
+                <Table pagination={false} columns={columns} dataSource={_data} />
+            </div>
         </div>
     );
 }
