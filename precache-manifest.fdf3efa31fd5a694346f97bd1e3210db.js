0b98bbb770d230370fd99a13ddbe2fcf161b50f9
idanhakim <idanhakim123@gmail.com>
2020-11-16 21:00:24 +0200

    Updates

---
+++ precache-manifest.fdf3efa31fd5a694346f97bd1e3210db.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0782d434e5ffe35856c802a8062db27",
+    "url": "/Social_Project/index.html"
+  },
+  {
+    "revision": "39ad990985488c14b2dd",
+    "url": "/Social_Project/static/css/2.432f1e83.chunk.css"
+  },
+  {
+    "revision": "a1c155dea0e9875db730",
+    "url": "/Social_Project/static/css/main.2a02d898.chunk.css"
+  },
+  {
+    "revision": "39ad990985488c14b2dd",
+    "url": "/Social_Project/static/js/2.625262ac.chunk.js"
+  },
+  {
+    "revision": "43df54f35a92efb402975cd2434cad56",
+    "url": "/Social_Project/static/js/2.625262ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1c155dea0e9875db730",
+    "url": "/Social_Project/static/js/main.f00d33ee.chunk.js"
+  },
+  {
+    "revision": "0f5a511b22455a9fa423",
+    "url": "/Social_Project/static/js/runtime-main.53aa5759.js"
+  },
+  {
+    "revision": "1d0855d19ce6a05111dab64f61066467",
+    "url": "/Social_Project/static/media/Pic1.1d0855d1.jpeg"
+  },
+  {
+    "revision": "9fc978ce1ff0d51b092a5d5d77d0b8c4",
+    "url": "/Social_Project/static/media/Pic2.9fc978ce.jpg"
+  },
+  {
+    "revision": "0824bc36c7f17205319343ef60f3abcb",
+    "url": "/Social_Project/static/media/Pic3.0824bc36.jpg"
+  },
+  {
+    "revision": "f5235c6dd881b1496f984b4b18c34146",
+    "url": "/Social_Project/static/media/account_balance-black-18dp.f5235c6d.svg"
+  },
+  {
+    "revision": "1fbaa23308c4a0622315803fdf11fa04",
+    "url": "/Social_Project/static/media/background-bright.1fbaa233.jpg"
+  },
+  {
+    "revision": "b06756b84714693f7564aa00c556844f",
+    "url": "/Social_Project/static/media/loginlogoimg.b06756b8.png"
+  },
+  {
+    "revision": "1f42ab865f0181b9c599fbd547c9516e",
+    "url": "/Social_Project/static/media/magnifying-glass.1f42ab86.svg"
+  },
+  {
+    "revision": "90a06e09ee3c9e088498634c67b2292a",
+    "url": "/Social_Project/static/media/school-black-18dp.90a06e09.svg"
+  },
+  {
+    "revision": "3473cffcf0c74d681e79030e34662d7d",
+    "url": "/Social_Project/static/media/url.3473cffc.svg"
+  }
+]);
