555741a2c702e86f03f1279205ffbbea9b9fc06f
idanhakim <idanhakim123@gmail.com>
2020-11-17 02:04:18 +0200

    redesign_personal_page

--- src/components/layout/PersonalPage/PersonalPage.jsx
+++ src/components/layout/PersonalPage/PersonalPage.jsx
@@ -2,23 +2,46 @@ import React from "react";
 import styles from "./PersonalPage.module.scss";
 import {UpdateDetailsForm} from "./UpdateDetailsForm/UpdateDetailsForm";
 
-export const PersonalPage = () =>{
+export const PersonalPage = () => {
+    const [isDetailsMode, setIsDetailsMode] = React.useState(true)
+
     return (
         <div className={styles.rootPersonalPage}>
-            <h1>איזור אישי</h1>
 
-            <div className={styles.container}>
-                <h2>עריכת פרטים אישיים:</h2>
-
-                <div className={styles.formWrapper}>
-                    <UpdateDetailsForm/>
-                </div>
-
+            <div className={styles.menu}>
+                <h2>איזור אישי</h2>
+                <ul>
+                    <li onClick={() => setIsDetailsMode(true)}>
+                        <span className={styles.icon}>icon</span>
+                        פרטים איישים
+                    </li>
+                    <li onClick={() => setIsDetailsMode(false)}>
+                        <span className={styles.icon}>icon</span>
+                        הפניות שלי
+                    </li>
+                </ul>
             </div>
 
-            <div className={styles.container}>
-                <h2>הפניות שלי:</h2>
+            <div className={styles.main}>
+                {isDetailsMode ?
+                    <div>שינוי פרטים</div>
+                    :
+                    <div>הפניות שלי</div>
+                }
             </div>
+
+            {/*<div className={styles.container}>*/}
+            {/*    <h2>עריכת פרטים אישיים:</h2>*/}
+
+            {/*    <div className={styles.formWrapper}>*/}
+            {/*        <UpdateDetailsForm/>*/}
+            {/*    </div>*/}
+
+            {/*</div>*/}
+
+            {/*<div className={styles.container}>*/}
+            {/*    <h2>הפניות שלי:</h2>*/}
+            {/*</div>*/}
         </div>
     );
 }
