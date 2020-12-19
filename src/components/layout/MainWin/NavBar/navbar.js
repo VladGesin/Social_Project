ff2436601704cd8139ce0a17a0098afc18a0e388
VladGesin <gasinvlad@gmail.com>
2020-12-19 14:19:56 +0200

    fixMargin

--- src/components/layout/MainWin/NavBar/navbar.js
+++ src/components/layout/MainWin/NavBar/navbar.js
@@ -1,45 +1,37 @@
-import React from "react";
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
-import "./navbar.css";
-import NavMapLinks from "./NavBarMapping/NavMapLinks";
-
+import React from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import './navbar.css';
+import NavMapLinks from './NavBarMapping/NavMapLinks';
 
 const XpertesyItems = {
-   link: [
-      {
-         name: "יצירת פגישה חדשה",
-         path: "/Social_Project/newmeeting",
-         item: "test",
-      },
-      { name: "הפגישות שלי", path: "/Social_Project/mymeetings", item: "test" },
-   ],
+	link: [
+		{
+			name: 'יצירת פגישה חדשה',
+			path: '/Social_Project/newmeeting',
+			item: 'test'
+		},
+		{ name: 'הפגישות שלי', path: '/Social_Project/mymeetings', item: 'test' }
+	]
 };
 
 const navbar = () => {
-   return (
-      <Navbar collapseOnSelect expand="lg" bg="light" variant="light" dir="rtl">
-         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-         <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="m-auto links-container">
-               <NavLink to="/Social_Project/committees">וועדות</NavLink>
-               <NavLink to="/Social_Project/maps">מפות</NavLink>
-               <NavDropdown
-                  title="Xpertesy"
-                  id="collasible-nav-dropdown"
-                  dir="rtl"
-                  className="text-right"
-               >
-                  {XpertesyItems.link.map((item) => (
-                     <NavMapLinks link={item} key={item.name} />
-                  ))}
-               </NavDropdown>
+	return (
+		<Navbar collapseOnSelect expand="lg" bg="light" variant="light" dir="rtl" className="mb-2">
+			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+			<Navbar.Collapse id="responsive-navbar-nav">
+				<Nav className="m-auto links-container">
+					<NavLink to="/Social_Project/committees">וועדות</NavLink>
+					<NavLink to="/Social_Project/maps">מפות</NavLink>
+					<NavDropdown title="Xpertesy" id="collasible-nav-dropdown" dir="rtl" className="text-right">
+						{XpertesyItems.link.map((item) => <NavMapLinks link={item} key={item.name} />)}
+					</NavDropdown>
 
-               <NavLink to="/Social_Project/SecretaryWin">מסך ניהול</NavLink>
-            </Nav>
-         </Navbar.Collapse>
-      </Navbar>
-   );
+					<NavLink to="/Social_Project/SecretaryWin">מסך ניהול</NavLink>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
+	);
 };
 
 export default navbar;
--- src/components/layout/MainWin/WelcomPage/GoodWord/GoodWordCarusel.js
+++ src/components/layout/MainWin/WelcomPage/GoodWord/GoodWordCarusel.js
@@ -1,45 +1,37 @@
-import React ,{useEffect,useState} from 'react'
-import Carousel from 'react-bootstrap/Carousel'
-import api from "../../../../../api";
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 
+const GoodWordCarusel = ({ goodWord }) => {
+	// const [goodWord,setGoodWord] = useState([]); //hook goodword
 
-const GoodWordCarusel=({goodWord})=> {
+	// useEffect(() => {
+	//   getGoodWord();
+	// }, [])
 
-// const [goodWord,setGoodWord] = useState([]); //hook goodword
+	// function getGoodWord() {
+	//   api.get('goodWord').then(res=>setGoodWord(res.data)
+	//   );
+	// }
+	const dateConvert = (time) => {
+		let d = time.toString().slice(0, 10).split('-');
+		let date = d[2] + '-' + d[1] + '-' + d[0]; // 10/30/2010
+		return date;
+	};
 
+	return (
+		<div>
+			<Carousel indicators={false} interval={2000}>
+				{goodWord.map((goodItem) => (
+					<Carousel.Item key={goodItem.serial_id} dir="rtl">
+						<h2>וועדה: {goodItem.committee_name}</h2>
+						<h3>לכבוד: {goodItem.serial_id}</h3>
+						<h5>תוכן: {goodItem.content}</h5>
+						<h6>{dateConvert(goodItem.time)}</h6>
+					</Carousel.Item>
+				))}
+			</Carousel>
+		</div>
+	);
+};
 
-  // useEffect(() => {
-  //   getGoodWord();
-  // }, [])
-
-  // function getGoodWord() {
-  //   api.get('goodWord').then(res=>setGoodWord(res.data)
-  //   );
-  // }
-  const dateConvert=(time)=>{
-    
-    let d = time.toString().slice(0, 10).split('-');   
-    let date = d[2] +'-'+ d[1]+'-' + d[0] // 10/30/2010
-    return date
-  }
-
-    return (
-      <div>
-        <Carousel indicators={false} interval={2000}>
-        {
-          goodWord.map( goodItem=>(
-            <Carousel.Item key={goodItem.serial_id} dir="rtl">
-            <h2>וועדה: {goodItem.committee_name}</h2>
-            <h3>לכבוד: {goodItem.serial_id}</h3>
-            <h5>תוכן: {goodItem.content}</h5>
-            <h6>{dateConvert(goodItem.time)}</h6>
-            </Carousel.Item>   
-          ))
-        }
-        </Carousel>
-      </div>
-    )
-  }
-
-
-export default GoodWordCarusel
+export default GoodWordCarusel;
