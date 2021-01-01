660faf4a92873aa198113d37cdb155c0388e2d94
Ron Benaish <r.benaish@gmail.com>
2021-01-01 13:23:13 +0200

    responsive pages

--- src/components/layout/MainWin/NavBar/NavBarMapping/NavMapLinks.js
+++ src/components/layout/MainWin/NavBar/NavBarMapping/NavMapLinks.js
@@ -1,26 +1,26 @@
-import React, { Fragment } from 'react';
-import { NavDropdown } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import React, { Fragment } from "react";
+import { NavDropdown } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 const NavMapLinks = (props) => {
-  return (
-    <Fragment>
-      <li>
-        {console.log(props.link.item)}
-        <NavDropdown.Item className="text-right">
-          {/* NavLink */}
-          <NavLink
-            to={{
-              pathname: `${props.link.path}/:${props.link.item}`,
-              state: { item: props.link.item },
-            }}
-          >
-            {props.link.name}
-          </NavLink>
-        </NavDropdown.Item>
-      </li>
-    </Fragment>
-  );
+   return (
+      <Fragment>
+         <li>
+            {console.log(props.link.item)}
+            <NavDropdown.Item className="text-right">
+               {/* NavLink */}
+               <NavLink
+                  to={{
+                     pathname: `${props.link.path}/:${props.link.item}`,
+                     state: { item: props.link.item },
+                  }}
+               >
+                  <span style={{ textAlign: "center" }}>{props.link.name}</span>
+               </NavLink>
+            </NavDropdown.Item>
+         </li>
+      </Fragment>
+   );
 };
 
 export default NavMapLinks;
--- src/components/layout/SecretaryWin/SecretaryWin.js
+++ src/components/layout/SecretaryWin/SecretaryWin.js
@@ -7,6 +7,7 @@ import MsgBox from "./MsgBox/MsgBox";
 
 const SecretaryWin = () => {
    const [msg, setMsg] = useState({ msg: "" });
+   const [mobileBoxIsOpen, setMobileBoxIsOpen] = useState(false);
    const [activePage, setActivePage] = useState({
       userManagement: true,
       contentManagement: false,
@@ -27,7 +28,105 @@ const SecretaryWin = () => {
                type={msg.type}
             />
          )}
-
+         <div className={style.mobileMenu}>
+            <div
+               className={style.users}
+               onClick={() => {
+                  setActivePage({
+                     userManagement: true,
+                     contentManagement: false,
+                     photos: false,
+                     goodWord: false,
+                     committees: false,
+                     contactPageManagement: false,
+                  });
+                  setMobileBoxIsOpen(false);
+               }}
+            >
+               ניהול משתמשים
+            </div>
+            <div className={style.content}>
+               <span
+                  onClick={() => {
+                     setMobileBoxIsOpen((cur) => !cur);
+                     setActivePage({
+                        userManagement: false,
+                        contentManagement: true,
+                        photos: true,
+                        goodWord: false,
+                        committees: false,
+                        contactPageManagement: false,
+                     });
+                  }}
+               >
+                  ניהול תוכן
+               </span>
+               {mobileBoxIsOpen && (
+                  <div className={style.mobileBox}>
+                     <span
+                        onClick={() => {
+                           setActivePage({
+                              userManagement: false,
+                              contentManagement: true,
+                              photos: true,
+                              goodWord: false,
+                              committees: false,
+                              contactPageManagement: false,
+                           });
+                           setMobileBoxIsOpen(false);
+                        }}
+                     >
+                        תמונות
+                     </span>
+                     <span
+                        onClick={() => {
+                           setActivePage({
+                              userManagement: false,
+                              contentManagement: true,
+                              photos: false,
+                              goodWord: true,
+                              committees: false,
+                              contactPageManagement: false,
+                           });
+                           setMobileBoxIsOpen(false);
+                        }}
+                     >
+                        מילה טובה
+                     </span>
+                     <span
+                        onClick={() => {
+                           setActivePage({
+                              userManagement: false,
+                              contentManagement: true,
+                              photos: false,
+                              goodWord: false,
+                              committees: true,
+                              contactPageManagement: false,
+                           });
+                           setMobileBoxIsOpen(false);
+                        }}
+                     >
+                        פניות לועדה
+                     </span>
+                     <span
+                        onClick={() => {
+                           setActivePage({
+                              userManagement: false,
+                              contentManagement: true,
+                              photos: false,
+                              goodWord: false,
+                              committees: false,
+                              contactPageManagement: true,
+                           });
+                           setMobileBoxIsOpen(false);
+                        }}
+                     >
+                        נהיול דף קשר
+                     </span>
+                  </div>
+               )}
+            </div>
+         </div>
          <div className={`${style.sideBar} ${style.hideOnMobile}`}>
             <SecretaryWinSideBar
                activePage={activePage}
