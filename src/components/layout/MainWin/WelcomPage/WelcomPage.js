f44c80da9b50d003dfe66cdb9f7b5d4c10747b4d
Arielle <65770037+ArielleMizrahi@users.noreply.github.com>
2021-02-09 15:45:25 +0200

    welcom page changes

--- src/components/layout/MainWin/WelcomPage/WelcomPage.js
+++ src/components/layout/MainWin/WelcomPage/WelcomPage.js
@@ -1,82 +1,91 @@
-import React, { Fragment, useEffect, useState } from "react";
-import Slider from "./Slider/Slider";
-import Container from "react-bootstrap/Container";
-import GoodWord from "../WelcomPage/GoodWord/GoodWord";
-import News from "../WelcomPage/News/News";
-import Birthday from "../WelcomPage/Birthdays/Birthday";
-import Teachers from "../WelcomPage/PrivateTeacher/PrivateTeacher";
-import { positions, Provider } from "react-alert";
-import AlertTemplate from "react-alert-template-basic";
-import MsgBox from "../../SecretaryWin/MsgBox/MsgBox";
+import React, { Fragment, useEffect, useState } from 'react';
+import Slider from './Slider/Slider';
+import Container from 'react-bootstrap/Container';
+import GoodWord from '../WelcomPage/GoodWord/GoodWord';
+import News from '../WelcomPage/News/News';
+import Birthday from '../WelcomPage/Birthdays/Birthday';
+import Teachers from '../WelcomPage/PrivateTeacher/PrivateTeacher';
+import { positions, Provider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
+import MsgBox from '../../SecretaryWin/MsgBox/MsgBox';
+import { Link } from 'react-router-dom';
+import { Message } from 'semantic-ui-react';
 
-import "./WelcomPage.css";
-import api from "../../../../api";
+
+import './WelcomPage.css';
+import api from '../../../../api';
 
 const WelcomPage = () => {
-  const [users, setUsers] = useState([]); //hook
-  const [news, setNews] = useState([]); // hook news
-  const [msg, setMsg] = useState({
-    msg: "",
-  });
-  const options = {
-    timeout: 3000,
-    position: positions.BOTTOM_CENTER,
-  };
-  useEffect(() => {
-    getUsers();
-    getNews("תלמיד");
-  }, []);
+	const [ users, setUsers ] = useState([]); //hook
+	const [ news, setNews ] = useState([]); // hook news
+	const [ msg, setMsg ] = useState({
+		msg: ''
+	});
+	const options = {
+		timeout: 3000,
+		position: positions.BOTTOM_CENTER
+	};
+	useEffect(() => {
+		getUsers();
+		getNews('תלמיד');
+	}, []);
 
-  function getUsers() {
-    api.get("users").then((res) => setUsers(res.data));
-  }
+	function getUsers() {
+		api.get('users').then((res) => setUsers(res.data));
+	}
 
-  function getNews(key) {
-    api.get("news?filterBy=${key}").then((res) => setNews(res.data));
-  }
+	function getNews(key) {
+		api.get('news?filterBy=${key}').then((res) => setNews(res.data));
+	}
 
-  return (
-    <Fragment>
-      <Provider template={AlertTemplate} {...options}>
-        <Container>
-          <div className="row justify-content-md-center">
-            <div className="col-md-8 ">
-              <div className="row d-block ">
-                <Slider />
-              </div>
-              {this.context.isExpired() && (
-							<p>
-								<Message>
-									<h14>
-										<span> הסיסמא עומדת לפוג בעוד .. ימים. לחץ כאן לשנות סיסמה.  </span>
-									</h14>
-								</Message>
-							</p>
-						)}
-              {msg.msg !== "" && (
-                <MsgBox
-                  name={msg.name}
-                  msg={msg.msg}
-                  clear={() => {
-                    setMsg({ msg: "" });
-                  }}
-                  type={msg.type}
-                />
-              )}
-              <div className="row d-block  ">
-                <GoodWord setMsg={setMsg} />
-              </div>
-            </div>
-            <div className="col">
-              <News news={news} />
-              <Birthday users={users} />
-              <Teachers />
-            </div>
-          </div>
-        </Container>
-      </Provider>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<Provider template={AlertTemplate} {...options}>
+				<Container>
+					<div className="row justify-content-md-center">
+						<div className="col-md-8 ">
+							<div className="row d-block ">
+								<Slider />
+							</div>
+							{this.context.isExpired() && (
+								<p>
+									<Message>
+										<h14>
+											<span>
+												הסיסמא עומדת לפוג בעוד ... ימים.
+												<Link to="/Social_Project/ForgotPassword">
+													<p> לחץ כאן </p>
+												</Link>
+												לשנות סיסמה.
+											</span>
+										</h14>
+									</Message>
+								</p>
+							)}
+							{msg.msg !== '' && (
+								<MsgBox
+									name={msg.name}
+									msg={msg.msg}
+									clear={() => {
+										setMsg({ msg: '' });
+									}}
+									type={msg.type}
+								/>
+							)}
+							<div className="row d-block  ">
+								<GoodWord setMsg={setMsg} />
+							</div>
+						</div>
+						<div className="col">
+							<News news={news} />
+							<Birthday users={users} />
+							<Teachers />
+						</div>
+					</div>
+				</Container>
+			</Provider>
+		</Fragment>
+	);
 };
 
 export default WelcomPage;
