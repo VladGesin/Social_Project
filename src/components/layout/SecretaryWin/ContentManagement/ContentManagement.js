3e51859664d156245e39d09873867e4c2610fa20
Ron Benaish <r.benaish@gmail.com>
2021-03-09 19:10:02 +0200

    contact page

--- src/components/layout/SecretaryWin/ContentManagement/ContentManagement.js
+++ src/components/layout/SecretaryWin/ContentManagement/ContentManagement.js
@@ -2,6 +2,7 @@ import React from "react";
 import Photos from "./Photos/Photos";
 import Committees from "./Committees/Committees";
 import GoodWord from "./GoodWord/GoodWord";
+import Contact from "./Contact/Contact";
 import style from "../SecretaryWin.module.scss";
 const ContentManagement = ({ activePage, setMsg }) => {
    return (
@@ -9,6 +10,7 @@ const ContentManagement = ({ activePage, setMsg }) => {
          {activePage.photos && <Photos setMsg={setMsg} />}
          {activePage.committees && <Committees setMsg={setMsg} />}
          {activePage.goodWord && <GoodWord setMsg={setMsg} />}
+         {activePage.contactPageManagement && <Contact setMsg={setMsg} />}
       </div>
    );
 };
