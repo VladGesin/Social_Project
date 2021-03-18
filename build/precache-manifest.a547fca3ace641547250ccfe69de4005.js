43497d33ddcddfa495868c4118fc18910cce1725
idanhakim <idanhakim123@gmail.com>
2021-03-18 02:32:04 +0200

    fix sort by status

---
+++ build/precache-manifest.a547fca3ace641547250ccfe69de4005.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9012d2f7db8d3ac2c3ff1406e3f867bb",
+    "url": "/Social_Project/index.html"
+  },
+  {
+    "revision": "6d5d5469917ec3e52a26",
+    "url": "/Social_Project/static/css/2.432f1e83.chunk.css"
+  },
+  {
+    "revision": "b1fceb7b36f0b50b552c",
+    "url": "/Social_Project/static/css/main.ce584145.chunk.css"
+  },
+  {
+    "revision": "6d5d5469917ec3e52a26",
+    "url": "/Social_Project/static/js/2.bc69299b.chunk.js"
+  },
+  {
+    "revision": "8d0007678260138e4c55530be1bdf0e2",
+    "url": "/Social_Project/static/js/2.bc69299b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1fceb7b36f0b50b552c",
+    "url": "/Social_Project/static/js/main.6672442f.chunk.js"
+  },
+  {
+    "revision": "0f5a511b22455a9fa423",
+    "url": "/Social_Project/static/js/runtime-main.53aa5759.js"
+  },
+  {
+    "revision": "f5235c6dd881b1496f984b4b18c34146",
+    "url": "/Social_Project/static/media/account_balance-black-18dp.f5235c6d.svg"
+  },
+  {
+    "revision": "1fbaa23308c4a0622315803fdf11fa04",
+    "url": "/Social_Project/static/media/background-bright.1fbaa233.jpg"
+  },
+  {
+    "revision": "70ac1623c4a67884ec452341067fc8e6",
+    "url": "/Social_Project/static/media/down-arrow.70ac1623.svg"
+  },
+  {
+    "revision": "f13b25b6e323fcf649cf7417c7685656",
+    "url": "/Social_Project/static/media/loginlogoimg.f13b25b6.png"
+  },
+  {
+    "revision": "1f42ab865f0181b9c599fbd547c9516e",
+    "url": "/Social_Project/static/media/magnifying-glass.1f42ab86.svg"
+  },
+  {
+    "revision": "2aa64ca8fad9a9fd40fabaaf0e412b97",
+    "url": "/Social_Project/static/media/multiple-users-silhouette.2aa64ca8.svg"
+  },
+  {
+    "revision": "90a06e09ee3c9e088498634c67b2292a",
+    "url": "/Social_Project/static/media/school-black-18dp.90a06e09.svg"
+  },
+  {
+    "revision": "ecef733a33ceec8dc32d29510998afe9",
+    "url": "/Social_Project/static/media/search.ecef733a.svg"
+  },
+  {
+    "revision": "7e19eab305b630026be0f8f561a01730",
+    "url": "/Social_Project/static/media/settings.7e19eab3.svg"
+  },
+  {
+    "revision": "3473cffcf0c74d681e79030e34662d7d",
+    "url": "/Social_Project/static/media/url.3473cffc.svg"
+  }
+]);
