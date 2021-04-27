ebb64a0d04291ac001835c25dab3a2d3017c11ec
idanhakim <idanhakim123@gmail.com>
2021-04-27 23:49:03 +0300

    add survey

--- src/components/layout/SurveysPage/SurveysList/index.jsx
+++ src/components/layout/SurveysPage/SurveysList/index.jsx
@@ -1,16 +1,41 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import styles from "./styles.module.scss";
 import api from "../../../../api";
 
-export const SurveysList = () =>{
+export const SurveysList = () => {
+
+    const [surveysList, setSurveysList] = useState(null)
+    const [showActiveOnly, setShowActiveOnly] = useState(true)
+    const [isLoading, setIsLoading] = useState(true)
+
 
     useEffect(() => {
-        api.get(`/survey/allSurvey/`)
-            .then((res) =>{
-                debugger
-            } )
-    } ,[]);
+        setIsLoading(true)
+        api.get(`/survey/${showActiveOnly ? 'getAllActiveSurvey' : 'allSurvey'}/`)
+            .then(({data}) => {
+                setIsLoading(false)
+                alert(JSON.stringify(data, null, 2))
+            })
+    }, [showActiveOnly]);
+
+    const handleChangeOnlyActiveFilter = (e) => {
+        setShowActiveOnly(e.target.checked)
+    }
     return (
-        <div>SurveysList</div>
+        <div>
+            <h1>רשימת סקרים:</h1>
+
+            <div>
+                <label>פעילים בלבד</label>
+                <input disabled={isLoading} type={'checkbox'} checked={showActiveOnly} onChange={handleChangeOnlyActiveFilter}/>
+            </div>
+
+            <div>
+                {
+                    !isLoading ? <div>רשימת סקרים</div> : <div>Loading...</div>
+                }
+            </div>
+
+        </div>
     );
 }
