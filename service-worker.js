21d3f049fe3e64fdd2f9c74b739f8bc83b594ee9
idanhakim <idanhakim123@gmail.com>
2021-04-27 21:53:55 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Social_Project/precache-manifest.05d6c86cc3b00598e8e8ea7b717db92c.js"
+  "/Social_Project/precache-manifest.bba20706402e7f4066df3b0e9643f9d8.js"
 );
 
 self.addEventListener('message', (event) => {
