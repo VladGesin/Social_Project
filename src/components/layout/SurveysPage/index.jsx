cc8e974f9bda8d8263aaa4ed0af071d034f44236
idanhakim <idanhakim123@gmail.com>
2021-04-28 00:00:29 +0300

    add survey

--- src/components/layout/SurveysPage/index.jsx
+++ src/components/layout/SurveysPage/index.jsx
@@ -5,24 +5,31 @@ import {SurveysList} from "./SurveysList";
 
 export const SurveysPage = () => {
 
-    const [isReviewSurveysMode, setIsReviewSurveysMode] = useState(false)
+    const [mode, setMode] = useState('add')
+
+    const getMainComp = () =>{
+        switch (mode){
+            case 'add':
+                return <AddNewSurvey/>
+            case 'list':
+                return <SurveysList/>
+            case 'vote':
+                return <div>הצבעות</div>
+
+        }
+    }
 
     return (
         <div className={styles.rootSurveysPage}>
             <h1>סקרים והצבעות</h1>
 
             <div className={styles.mainPage}>
-                <div className={styles.main}>
-                    {isReviewSurveysMode ?
-                        <SurveysList/>
-                        :
-                        <AddNewSurvey/>
-                    }
-                </div>
+                <div className={styles.main}>{getMainComp()}</div>
 
                 <div className={styles.menu}>
-                    <button onClick={() => setIsReviewSurveysMode(false)}>הוספת סקר</button>
-                    <button onClick={() => setIsReviewSurveysMode(true)}>צפייה בסקרים</button>
+                    <button onClick={() => setMode('add')}>הוספת סקר</button>
+                    <button onClick={() => setMode('list')}>צפייה בסקרים</button>
+                    <button onClick={() => setMode('vote')}>הצבעה בסקר</button>
                 </div>
             </div>
 
